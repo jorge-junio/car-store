86929cb3eac3acbe9dbe4a793ff9238ef1399579
Jorge Silva <jjunio.profissional@gmail.com>
2026-03-30 17:20:47 -0300

    Iniciando a trocar as view as function por class view

--- cars/views.py
+++ cars/views.py
@@ -1,24 +1,46 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
 
-# esta forma é muito trabalhosa, deixamos de usar esta classe
-def cars_view(request):
-    search = request.GET.get('search')
-    # ao passar o order_by se quiser ordenar decrescente é só
-    # colocar um '-' antes do nome do campo. Exemplo: order_by('-model')
-    if search:
-        # busca os dados do banco de dados e salva na variável cars
-        cars = Car.objects.filter(model__icontains=search).order_by('model')
-    else:
-        cars = Car.objects.all().order_by('model')
+# método usando function as view
+# def cars_view(request):
+#     search = request.GET.get('search')
+#     # ao passar o order_by se quiser ordenar decrescente é só
+#     # colocar um '-' antes do nome do campo. Exemplo: order_by('-model')
+#     if search:
+#         # busca os dados do banco de dados e salva na variável cars
+#         cars = Car.objects.filter(model__icontains=search).order_by('model')
+#     else:
+#         cars = Car.objects.all().order_by('model')
 
-    return render(
-        request=request,
-        template_name='cars.html',
-        context={'cars': cars}
-    )
+#     return render(
+#         request=request,
+#         template_name='cars.html',
+#         context={'cars': cars}
+#     )
+
+
+# método usando class view
+class CarsView(View):
+
+    def get(self, request):
+        search = request.GET.get('search')
+        # ao passar o order_by se quiser ordenar decrescente é só
+        # colocar um '-' antes do nome do campo. Exemplo: order_by('-model')
+        if search:
+            # busca os dados do banco de dados e salva na variável cars
+            cars = Car.objects.filter(
+                model__icontains=search).order_by('model')
+        else:
+            cars = Car.objects.all().order_by('model')
+
+        return render(
+            request=request,
+            template_name='cars.html',
+            context={'cars': cars}
+        )
 
 
 def new_car_view(request):
