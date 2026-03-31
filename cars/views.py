cb1bfadfef61f237d0a572968fe3e07406551a4d
Jorge Silva <jjunio.profissional@gmail.com>
2026-03-31 12:43:22 -0300

    Remove functions as view que estavam comentadas

--- cars/views.py
+++ cars/views.py
@@ -4,24 +4,6 @@ from cars.forms import CarModelForm
 from django.views import View
 
 
-# método usando function as view
-# def cars_view(request):
-#     search = request.GET.get('search')
-#     # ao passar o order_by se quiser ordenar decrescente é só
-#     # colocar um '-' antes do nome do campo. Exemplo: order_by('-model')
-#     if search:
-#         # busca os dados do banco de dados e salva na variável cars
-#         cars = Car.objects.filter(model__icontains=search).order_by('model')
-#     else:
-#         cars = Car.objects.all().order_by('model')
-
-#     return render(
-#         request=request,
-#         template_name='cars.html',
-#         context={'cars': cars}
-#     )
-
-
 # método usando class view
 class CarsView(View):
 
@@ -41,22 +23,6 @@ class CarsView(View):
             template_name='cars.html',
             context={'cars': cars}
         )
-
-
-# método usando function as view
-# def new_car_view(request):
-#     if request.method == 'POST':
-#         new_car_form = CarModelForm(request.POST, request.FILES)
-#         if new_car_form.is_valid():
-#             new_car_form.save()
-#             return redirect('cars_list')
-#     else:
-#         new_car_form = CarModelForm()
-#     return render(
-#         request=request,
-#         template_name='new_car.html',
-#         context={'new_car_form': new_car_form}
-#     )
 
 
 # método usando class view
