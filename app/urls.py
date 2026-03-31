a4f4e4f70e3a2e9fb8b4b6dfe1dbc86f9a354fbd
Jorge Silva <jjunio.profissional@gmail.com>
2026-03-31 12:57:18 -0300

    Substituimos a listagem de carros com View para a forma usando ListView

--- app/urls.py
+++ app/urls.py
@@ -19,13 +19,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import register_view, login_view, logout_view
-from cars.views import CarsView, NewCarView
+from cars.views import NewCarView, CarsListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('cars/', CarsView.as_view(), name='cars_list'),
+    path('cars/', CarsListView.as_view(), name='cars_list'),
     path('new_car/', NewCarView.as_view(), name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -2,27 +2,24 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView
 
 
-# método usando class view
-class CarsView(View):
+# usando ListView
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
-    def get(self, request):
-        search = request.GET.get('search')
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
         # ao passar o order_by se quiser ordenar decrescente é só
         # colocar um '-' antes do nome do campo. Exemplo: order_by('-model')
         if search:
             # busca os dados do banco de dados e salva na variável cars
-            cars = Car.objects.filter(
-                model__icontains=search).order_by('model')
-        else:
-            cars = Car.objects.all().order_by('model')
-
-        return render(
-            request=request,
-            template_name='cars.html',
-            context={'cars': cars}
-        )
+            cars = cars.filter(model__icontains=search)
+        return cars
 
 
 # método usando class view
