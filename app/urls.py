67a203da30fecf5620590eb16e1a166682940f94
Jorge Silva <jjunio.profissional@gmail.com>
2026-03-31 14:30:21 -0300

    Criamos a view de detalhes do carro

--- app/urls.py
+++ app/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import register_view, login_view, logout_view
-from cars.views import NewCarCreateView, CarsListView
+from cars.views import NewCarCreateView, CarsListView, CarDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +28,5 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('cars/', CarsListView.as_view(), name='cars_list'),
     path('new_car/', NewCarCreateView.as_view(), name='new_car'),
+    path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
